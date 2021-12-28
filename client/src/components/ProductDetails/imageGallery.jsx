d323e262116d6b0ef413247fe97582aad5a16d60
John Razi <gc.johnrazi@gmail.com>
2021-12-28 11:20:02 -0800

    addToCart basic outline render

---
+++ client/src/components/ProductDetails/imageGallery.jsx
@@ -0,0 +1,30 @@
+import React/*, {useState}*/ from 'react';
+
+class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+    };
+  }
+  clickHandler() {
+
+  }
+
+  render() {
+    return (
+      <>
+        <img src=""></img>
+        {}
+      </>
+    );
+  }
+}
+
+
+
+
+
+
+
+export default ImageGallery;
