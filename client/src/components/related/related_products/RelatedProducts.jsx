e073ad29ec2639c45ecebf9ef9b36d569915c213
Meilin <immeilin9@gmail.com>
2021-12-29 11:41:12 -0800

    fixed import warning in outfits

--- client/src/components/related/related_products/RelatedProducts.jsx
+++ client/src/components/related/related_products/RelatedProducts.jsx
@@ -1,10 +1,9 @@
-import React, {useState, useContext, useEffect, useMemo} from 'react';
-import sampleData from '../sampleData.js';
-import Modal from '../modal/Modal.jsx';
+import React, { useState, useContext, useEffect, useMemo } from "react";
+import sampleData from "../sampleData.js";
+import Modal from "../modal/Modal.jsx";
 // import './RelatedProducts.css';
 
 const RelatedProducts = () => {
-
   const [relatedProducts, setRelatedProducts] = useState(sampleData);
   return (
     <>
@@ -13,14 +12,18 @@ const RelatedProducts = () => {
         {relatedProducts.map((item) => (
           <div className="card" key={item.id}>
             <div className="card__body">
-              <img className="card__image" src={item.photos[0].thumbnail_url} width="100" height="100"/>
+              <img
+                className="card__image"
+                src={item.photos[0].thumbnail_url}
+                width="100"
+                height="100"
+              />
               <div className="card__category">{item.category}</div>
               <div className="card__name">{item.name}</div>
               <div className="card__price">${item.default_price}</div>
               <div className="card__rate">rate: {item.rate[0]}</div>
             </div>
           </div>
-
         ))}
       </div>
     </>
