2b1eb7f1b3823516079a57dfae8420d148020f30
Meilin <immeilin9@gmail.com>
2021-12-29 01:19:09 -0800

    added related product cards css styles, need to fix loader

--- client/src/components/related/related_products/RelatedProducts.jsx
+++ client/src/components/related/related_products/RelatedProducts.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useContext, useEffect, useMemo} from 'react';
 import sampleData from '../sampleData.js';
 import Modal from '../modal/Modal.jsx';
+// import './RelatedProducts.css';
 
 const RelatedProducts = () => {
 
@@ -10,13 +11,16 @@ const RelatedProducts = () => {
       <h1>Related Products</h1>
       <div className="relatedProductsContainer">
         {relatedProducts.map((item) => (
-          <div className="card__body" key={item.id}>
-            <img className="card__pic" src={item.photos[0].thumbnail_url}/>
-            <div className="card__category">{item.category}</div>
-            <div className="card__name">{item.name}</div>
-            <div className="card__price">${item.default_price}</div>
-            <div className="card__rate">rate: {item.rate[0]}</div>
+          <div className="card" key={item.id}>
+            <div className="card__body">
+              <img className="card__image" src={item.photos[0].thumbnail_url} width="100" height="100"/>
+              <div className="card__category">{item.category}</div>
+              <div className="card__name">{item.name}</div>
+              <div className="card__price">${item.default_price}</div>
+              <div className="card__rate">rate: {item.rate[0]}</div>
+            </div>
           </div>
+
         ))}
       </div>
     </>
