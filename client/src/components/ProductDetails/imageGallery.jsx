7c5c759289c3a277c03b4d9246c427c47f391700
John Razi <gc.johnrazi@gmail.com>
2021-12-29 11:02:36 -0800

    Started rudimentary server

--- client/src/components/ProductDetails/imageGallery.jsx
+++ client/src/components/ProductDetails/imageGallery.jsx
@@ -1,26 +1,31 @@
-import React/*, {useState}*/ from 'react';
+import React /*, {useState}*/ from 'react';
 
 class ImageGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: ['https://upload.wikimedia.org/wikipedia/commons/b/bc/Juvenile_Ragdoll.jpg',
+      images: [
+        'https://upload.wikimedia.org/wikipedia/commons/b/bc/Juvenile_Ragdoll.jpg',
         'https://www.petmd.com/sites/default/files/styles/article_image/public/small-kitten-walking-towards_127900829_0.jpg?itok=ah_gTtbS',
         'https://ichef.bbci.co.uk/news/976/cpsprodpb/1852E/production/_121203699_2.63235180.jpg',
         'https://www.comfortzone.com/-/media/Images/ComfortZone-NA/US/Blog/bringing-new-kitten-home.jpg',
         'https://spca.bc.ca/wp-content/uploads/news-kittens.jpg',
-        'https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png'],
+        'https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png',
+      ],
       selected: null,
-      index: null
+      index: null,
     };
   }
 
   componentDidMount() {
-    this.setState({selected: this.state.images[0], index: 0});
+    this.setState({ selected: this.state.images[0], index: 0 });
   }
 
   clickHandler(event) {
-    this.setState({selected: event.target.src, index: this.state.images.indexOf(event.target.src)});
+    this.setState({
+      selected: event.target.src,
+      index: this.state.images.indexOf(event.target.src),
+    });
   }
 
   imgChange(event) {
@@ -29,32 +34,48 @@ class ImageGallery extends React.Component {
       if (index === 0) {
         index = this.state.images.length;
       }
-      this.setState({selected: this.state.images[index - 1], index: index - 1});
+      this.setState({
+        selected: this.state.images[index - 1],
+        index: index - 1,
+      });
     } else {
       if (index === this.state.images.length - 1) {
         index = -1;
       }
-      this.setState({selected: this.state.images[index + 1], index: index + 1});
+      this.setState({
+        selected: this.state.images[index + 1],
+        index: index + 1,
+      });
     }
-    // this.setState()
   }
 
   render() {
     return (
       <>
-        <img id="big_image" value={this.state.index} src={this.state.selected}></img>
-        {this.state.images.map(item => {
+        <img
+          id='big_image'
+          value={this.state.index}
+          src={this.state.selected}
+        ></img>
+        {this.state.images.map((item) => {
           return (
-            <img onClick={() => this.clickHandler(event)} src={item} key={item} className="thumbnail"></img>
+            <img
+              onClick={() => this.clickHandler(event)}
+              src={item}
+              key={item}
+              className='thumbnail'
+            ></img>
           );
         })}
-        <button id="prev" onClick={() => this.imgChange(event)}>Prev</button><button id="next" onClick={() => this.imgChange(event)}>Next</button>
+        <button id='prev' onClick={() => this.imgChange(event)}>
+          Prev
+        </button>
+        <button id='next' onClick={() => this.imgChange(event)}>
+          Next
+        </button>
       </>
     );
   }
 }
 
 export default ImageGallery;
-
-
-
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,18 +1,39 @@
-import React/*, {useState}*/ from 'react';
+import React from 'react';
 
-import {AddToCart, ProductInfo, ImageGallery} from './ProductDetails/expandedInfo.js';
+import {
+  AddToCart,
+  ProductInfo,
+  ImageGallery,
+} from './ProductDetails/expandedInfo.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      currentItem: {
+        id: 40344,
+        data: 0, //
+        related: [], //
+      },
+      allProducts: [{}], //load all products and store features/styles in each obj
+    };
+  }
+
+  populateData() {}
+
+  clickHandler() {
+    this.setState({ currentItem: 0 });
   }
 
   render() {
     return (
       <>
-        <div><ImageGallery /><AddToCart /></div>
+        AddContext
+        <div>
+          <ImageGallery />
+          <AddToCart />
+        </div>
         <ProductInfo />
       </>
     );
