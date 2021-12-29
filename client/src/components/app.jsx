ab981e6d5fe0358d44acce86def9cf096d952f51
Jacob Hawkins <jacobhawkins7@gmail.com>
2021-12-29 11:11:37 -0800

    save before main upstream sync

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import Ratings from './Ratings.jsx';
-import Reviews from './Reviews.jsx';
+import React, { useState } from "react";
+import Ratings from "./Ratings.jsx";
+import Reviews from "./Reviews.jsx";
 
 function App() {
-
   return (
-    <div>Yahtzee!
-    <Ratings />
-    <Reviews />
+    <div>
+      Yahtzee!
+      <Ratings />
+      <Reviews />
     </div>
-  )
+  );
 }
 
 export default App;
