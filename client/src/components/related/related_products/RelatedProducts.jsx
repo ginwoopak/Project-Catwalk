37f75b4fba64659e8f55d44d9ac891526968dd87
Meilin <immeilin9@gmail.com>
2021-12-29 17:11:07 -0800

    remove dots under carousel

--- client/src/components/related/related_products/RelatedProducts.jsx
+++ client/src/components/related/related_products/RelatedProducts.jsx
@@ -34,7 +34,7 @@ const RelatedProducts = () => {
           arrows={true}
           swipeable={false}
           draggable={false}
-          showDots={true}
+          showDots={false}
           responsive={responsive}
           ssr={true} // means to render carousel on server-side.
           // infinite={true}
@@ -49,7 +49,6 @@ const RelatedProducts = () => {
           // dotListClass="custom-dot-list-style"
           // itemClass="carousel-item-padding-40-px"
         >
-          {/* <div className="gridContainer"> */}
           {relatedProducts.map((item) => (
             <div className="card" key={item.id}>
               <div className="card__body">
@@ -66,27 +65,8 @@ const RelatedProducts = () => {
               </div>
             </div>
           ))}
-          {/* </div> */}
         </Carousel>
       </div>
-      {/* <div className="gridContainer">
-        {relatedProducts.map((item) => (
-          <div className="card" key={item.id}>
-            <div className="card__body">
-              <img
-                className="card__image"
-                src={item.photos[0].thumbnail_url}
-                width="100"
-                height="100"
-              />
-              <div className="card__category">{item.category}</div>
-              <div className="card__name">{item.name}</div>
-              <div className="card__price">${item.default_price}</div>
-              <div className="card__rate">rate: {item.rate[0]}</div>
-            </div>
-          </div>
-        ))}
-      </div> */}
     </>
   );
 };
